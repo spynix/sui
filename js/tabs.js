9d93e11dd25044578c3a20297b3e746056e1d65c
spynix <spynix@users.noreply.github.com>
2015-08-18 17:31:49 -0500

    Updated tabs to handle persistent viewport content

--- js/tabs.js
+++ js/tabs.js
@@ -39,6 +39,143 @@ define(["jquery"], function($) {
   var tabs_module = (function() {
     var top_control_id = 0;
     
+    function create_control(controller, viewport, top_id) {
+      return (function(controller, viewport, top_id) {
+        var id = top_id;
+        var top_tab_id = 0;
+        var tablist = [];
+
+        var attach_event = function(element, event, f) {
+          element[event] = f;
+        };
+
+        return {
+          id: function() {
+            return id;
+          },
+
+          top_tab_id: function() {
+            return top_tab_id;
+          },
+
+          add_tab: function(title, content) {
+            var tab = document.createElement("div");
+
+            if (!title || !content)
+              return false;
+
+            tab.id = "tab_" + id.toString() + "_" + top_tab_id.toString();
+            tab.className = "tab";
+            tab.innerHTML = title;
+
+            controller.appendChild(tab);
+            tablist.push(tab);
+            top_tab_id++;
+
+            attach_event(tab, "onclick", (function(controller, viewport, content) {
+              return function () {
+                /* this is a shittier method than i wanted for removing the
+                 * active class from the other tabs with, but the alternatives
+                 * i thought up made it way more complex so i said fuck that
+                 * shit.  i'd like to revisit this and come up with a better
+                 * solution in the future.
+                 */
+                $(controller).find(".active").removeClass("active");
+                $(this).addClass("active");
+                viewport.innerHTML = content;
+              };
+            })(controller, viewport, content));
+          },
+
+          remove_tab: function(index) {
+            controller.removeChild(tablist[index]);
+            tablist.splice(index, 1);
+          },
+
+          get_tab: function(index) {
+            return tablist[index];
+          },
+
+          get_tabs: function() {
+            return tablist;
+          }
+        };
+      })(controller, viewport, top_id);
+    }
+    
+    function create_persistent_control(controller, viewport, top_id) {
+      return (function(controller, viewport, top_id) {
+        var id = top_id;
+        var top_tab_id = 0;
+        var tablist = [];
+
+        var attach_event = function(element, event, f) {
+          element[event] = f;
+        };
+
+        return {
+          id: function() {
+            return id;
+          },
+
+          top_tab_id: function() {
+            return top_tab_id;
+          },
+
+          add_tab: function(title, content) {
+            var tab = document.createElement("div");
+            var view = document.createElement("div");
+
+            if (!title || !content)
+              return false;
+
+            tab.id = "tab_" + id.toString() + "_" + top_tab_id.toString();
+            tab.className = "tab";
+            tab.innerHTML = title;
+            
+            view.id = "tab_" + id.toString() + "_" + top_tab_id.toString() + "_view";
+            view.className = "viewport-persistence";
+            view.innerHTML = content;
+
+            controller.appendChild(tab);
+            tablist.push(tab);
+            top_tab_id++;
+            
+            viewport.appendChild(view);
+
+            attach_event(tab, "onclick", (function(controller, viewport, view) {
+              return function () {
+                /* this is a shittier method than i wanted for removing the
+                 * active class from the other tabs with, but the alternatives
+                 * i thought up made it way more complex so i said fuck that
+                 * shit.  i'd like to revisit this and come up with a better
+                 * solution in the future.
+                 */
+                $(controller).find(".active").removeClass("active");
+                $(this).addClass("active");
+                
+                $(viewport).find(".viewport-persistence.active").removeClass("active");
+                $(view).addClass("active");
+              };
+            })(controller, viewport, view));
+          },
+
+          remove_tab: function(index) {
+            controller.removeChild(tablist[index]);
+            tablist.splice(index, 1);
+          },
+
+          get_tab: function(index) {
+            return tablist[index];
+          },
+
+          get_tabs: function() {
+            return tablist;
+          }
+        };
+      })(controller, viewport, top_id);
+    }
+    
     return {
       create: function(controller, viewport, tabs, config) {
         var control, i, l;
@@ -49,67 +186,10 @@ define(["jquery"], function($) {
         $(controller).addClass("controls");
         $(viewport).addClass("viewport");
         
-        control = (function(controller, viewport, top_id) {
-          var id = top_id;
-          var top_tab_id = 0;
-          var tablist = [];
-          
-          var attach_event = function(element, event, f) {
-            element[event] = f;
-          };
-          
-          return {
-            id: function() {
-              return id;
-            },
-            
-            top_tab_id: function() {
-              return top_tab_id;
-            },
-            
-            add_tab: function(title, content) {
-              var tab = document.createElement("div");
-              
-              if (!title || !content)
-                return false;
-      
-              tab.id = "tab_" + id.toString() + "_" + top_tab_id.toString();
-              tab.className = "tab";
-              tab.innerHTML = title;
-      
-              controller.appendChild(tab);
-              tablist.push(tab);
-              top_tab_id++;
-              
-              attach_event(tab, "onclick", (function(controller, viewport, content) {
-                return function () {
-                  /* this is a shittier method than i wanted for removing the
-                   * active class from the other tabs with, but the alternatives
-                   * i thought up made it way more complex so i said fuck that
-                   * shit.  i'd like to revisit this and come up with a better
-                   * solution in the future.
-                   */
-                  $(controller).find(".active").removeClass("active");
-                  $(this).addClass("active");
-                  viewport.innerHTML = content;
-                };
-              })(controller, viewport, content));
-            },
-            
-            remove_tab: function(index) {
-              controller.removeChild(tablist[index]);
-              tablist.splice(index, 1);
-            },
-            
-            get_tab: function(index) {
-              return tablist[index];
-            },
-            
-            get_tabs: function() {
-              return tablist;
-            }
-          };
-        })(controller, viewport, top_control_id);
+        if (config.persistent === true)
+          control = create_persistent_control(controller, viewport, top_control_id);
+        else
+          control = create_control(controller, viewport, top_control_id);
         
         top_control_id++;
         
